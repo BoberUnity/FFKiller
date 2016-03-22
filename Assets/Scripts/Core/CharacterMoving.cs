41db78e7c5d11e5de273ea38a8b862042c9393f3
Дмитрий Вавилов <dvav@mail.ru>
2016-03-22 23:46:31 +0700

    Добавлена задержка реакции клавиатуры (экспериментально)

--- Assets/Scripts/Core/CharacterMoving.cs
+++ Assets/Scripts/Core/CharacterMoving.cs
@@ -11,6 +11,8 @@ public class CharacterMoving : MonoBehaviour
   private float currentSpeed = 0;
   private Animator thisAnimator = null;
   [HideInInspector] public bool CanMove = true;
+  public float HandlingDelay;
+  private float TimeToHandle;
 
     //DV{
     public SpriteRenderer CharSpriteRenderer;
@@ -28,17 +30,23 @@ public class CharacterMoving : MonoBehaviour
     void Update()
     {
         CharSpriteRenderer.sortingOrder = (int)(-transform.position.y * 2);
+        TimeToHandle -= Time.deltaTime;
+        if (TimeToHandle < 0f)
+        {
+            MovingHandling();
+            TimeToHandle = HandlingDelay;
+        }
     }
     //DV}
 
-    private void FixedUpdate ()
+    private void MovingHandling()
   {
     if (CanMove)
     {
       currentSpeed = Input.GetKey(KeyCode.RightShift) ? speedRun : speedWalk;
       if (Input.GetKey(KeyCode.UpArrow))
       {
-        transform.position += Vector3.up * Time.fixedDeltaTime * currentSpeed;
+        transform.position += Vector3.up * Time.deltaTime * currentSpeed;
         thisAnimator.SetBool("Running", true);
         thisAnimator.SetFloat("SpeedY", 1);
         if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
@@ -49,7 +57,7 @@ public class CharacterMoving : MonoBehaviour
 
       if (Input.GetKey(KeyCode.DownArrow))
       {
-        transform.position -= Vector3.up * Time.fixedDeltaTime * currentSpeed;
+        transform.position -= Vector3.up * Time.deltaTime * currentSpeed;
         thisAnimator.SetBool("Running", true);
         thisAnimator.SetFloat("SpeedY", -1);
         if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
@@ -60,7 +68,7 @@ public class CharacterMoving : MonoBehaviour
 
       if (Input.GetKey(KeyCode.RightArrow))
       {
-        transform.position += Vector3.right * Time.fixedDeltaTime * currentSpeed;
+        transform.position += Vector3.right * Time.deltaTime * currentSpeed;
         thisAnimator.SetBool("Running", true);
         thisAnimator.SetFloat("SpeedX", 1);
         if (!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
@@ -68,10 +76,10 @@ public class CharacterMoving : MonoBehaviour
           thisAnimator.SetFloat("SpeedY", 0);
         }
       }      
-
+      
       if (Input.GetKey(KeyCode.LeftArrow))
       {
-        transform.position -= Vector3.right * Time.fixedDeltaTime * currentSpeed;
+        transform.position -= Vector3.right * Time.deltaTime * currentSpeed;
         thisAnimator.SetBool("Running", true);
         thisAnimator.SetFloat("SpeedX", -1);
         if (!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
