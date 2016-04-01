c3d809b83f312d5098f6b2da367462361b6b0814
BoberUnity <borisberez@rambler.ru>
2016-04-02 02:20:58 +0300

    Описание элементов в Quest, Инвентарь берется из базы предметов

---
+++ Assets/Scripts/Events/AddToInventar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class AddToInventar: MonoBehaviour
+{
+  [SerializeField] private string itemName;
+  [SerializeField] private int count = 1;
+  private Inventar inventar = null;
+  private BaseOfInventar baseOfInventar = null;
+
+  private void Start()
+  {
+    inventar = FindObjectOfType<Inventar>();
+    baseOfInventar = FindObjectOfType<BaseOfInventar>(); ;
+  }
+
+  public void OnEventAction()
+  {
+    if (baseOfInventar != null)
+    {
+      foreach (var item in baseOfInventar.Items)
+      {
+        if (itemName == item.Name)
+        {
+          item.Count = count;
+          inventar.AddItem(item);
+        }
+      }
+    }
+    else
+    {
+      Debug.LogWarning("База инвентаря не загружена! Необходимо загрузить игру из сцены StartMenu");
+    }
+  }  
+}
