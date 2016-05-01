294260254a0d58874595f64bc97e28afa0238f7d
Дмитрий Вавилов <dvav@mail.ru>
2016-05-02 03:37:58 +0700

    Pivot спрайтов анимированных объектов, которые могут перекрывать друг друга (ГГ, трава) перенесены в нижнюю точку. На карту добавлена анимация травы. Добавлен скрипт для автоматического перекрытия спрайтов объектов (SortingLayer). Добавлен скрипт для случайного смещения стартового кадра анимации (для привнесения различия в одинаковые анимации)

---
+++ Assets/Scripts/Environment/VariedAnimations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class VariedAnimations : MonoBehaviour {
+	void Start () {
+        Animator Animator = GetComponent<Animator>();
+        Animator.SetFloat("Offset", Random.Range(0f, 1f));
+	}
+	
+}
