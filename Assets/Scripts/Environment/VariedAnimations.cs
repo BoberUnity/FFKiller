c508b804e78c027493fe2e329d40d233a86d1785
Дмитрий Вавилов <dvav@mail.ru>
2016-05-05 01:21:20 +0700

    На карту добавлено мнооооооооого анимированной травы.

--- Assets/Scripts/Environment/VariedAnimations.cs
+++ Assets/Scripts/Environment/VariedAnimations.cs
@@ -7,6 +7,8 @@ public class VariedAnimations : MonoBehaviour {
 	void Start () {
         Animator Animator = GetComponent<Animator>();
         Animator.SetFloat("Offset", Random.Range(0f, 1f));
-	}
+        Animator.SetFloat("SpeedModifier", Random.Range(0.9f, 1.1f));
+
+    }
 	
 }
